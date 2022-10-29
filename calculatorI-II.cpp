d7b3dc3fd70579e3c9ff1b646a5c73faff0d690f
SophioDatunashvili <115706962+SophioDatunashvili@users.noreply.github.com>
2022-10-29 17:18:20 +0400

    Third draft

--- calculatorI-II.cpp
+++ calculatorI-II.cpp
@@ -2,77 +2,82 @@
 
 using namespace std;
 
-int roman_to_integer(string rstr) {
-    int n = int(rstr.length());
-    if (0 == n) {
-        return 0;
+int arabicValue(char roman) 
+{
+    switch(roman) 
+    {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
     }
+    return 0;
+}
 
-    int result = 0;
-    for (int i = 0; i < n; i++) {
-        switch (rstr[i]) {
-            case 'I':
-                result += 1;
-                break;
-            case 'V':
-                result += 5;
-                break;
-            case 'X':
-                result += 10;
-                break;
-            case 'L':
-                result += 50;
-                break;
-            case 'C':
-                result += 100;
-                break;
-            case 'D':
-                result += 500;
-                break;
-            case 'M':
-                result += 1000;
-                break;
-            // default:
-            //     cout << "Invalid Number" << endl;
-            //     return 0;
-                
-            }
+
+int romanToInt(string romanNumber)
+{
+    int arabic = 0;
+    int previousNum = 0;
+    int n = romanNumber.length()-1;
+
+    for (int i = n; i >= 0; i--)
+    {
+        if (arabicValue(romanNumber[i]) >= previousNum) {
+             arabic = arabic + arabicValue(romanNumber[i]);
+        } else {
+            arabic = arabic - arabicValue(romanNumber[i]);
         }
 
-        for (int i = 1; i < n; i++) {
-            if ((rstr[i] == 'V' || rstr[i] == 'X') && rstr[i - 1] == 'I') {
-                result -= 1 + 1;
-            }
+        previousNum = arabicValue(romanNumber[i]);   
 
-            if ((rstr[i] == 'L' || rstr[i] == 'C') && rstr[i - 1] == 'X') {
-                result -= 10 + 10;
-            }
-
-            if ((rstr[i] == 'D' || rstr[i] == 'M') && rstr[i - 1] == 'C') {
-                result -= 100 + 100;
-            }
-        }
-
-        return result;
     }
+    return arabic;
+}
     
-    
-string integer_to_Roman(int n) {
+int calculator(string num, int result, string op) {
 
-    string str_romans[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-
-    string result = "";
-
-    for ( int i = 0; i < 13; ++i){
-
-        while(n - values[i] >= 0){
-            result += str_romans[i];
-            n -= values[i];
-        }
+    cout << "Enter Roman Number: ";
+    cin >> num;
+            
+    if (op=="+") {
+        result=result+romanToInt(num);
+    } else if (op=="-"){
+        result=result-romanToInt(num);
+    } else if (op=="*") {
+        result=result*romanToInt(num);
+    } else if(op=="/") {
+        result=result/romanToInt(num);
+    } else {
+        cout << "invalid operator";
     }
-
     return result;
+}
+
+    
+string intToRoman(int arabic) {
+    if (arabic < 0) {
+        arabic = -arabic;
+        cout << "-";
+    }
+
+    string romanChars[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    int arabicValues[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+    string roman = "";
+
+    for ( int i = 0; i < 13; i++){
+
+        while (arabic - arabicValues[i] >= 0){
+            roman = roman + romanChars[i];
+            arabic = arabic- arabicValues[i];
+        }
+    }
+
+    return roman;
 }
 
 
@@ -84,50 +89,20 @@ int main(){
     cout<<"Enter Roman Number: ";
     cin>>romanNumber;
 
-    int result = roman_to_integer(romanNumber);
+    int result = romanToInt(romanNumber);
     
     while(true) {
         string num;
         cout<<"enter operator: ";
         cin>>op;
-
-
+    
         if (op == "exit"){
             break;
-        }
-        
-        else{
-            cout << "Enter Roman Number: ";
-            cin >> num;
-            
-            if (op=="+") {
-
-                result=result+roman_to_integer(num);
-            } 
-            
-            else if (op=="-"){
-
-                result=result-roman_to_integer(num);
-            }
-            
-            else if (op=="*") {
-
-                result=result*roman_to_integer(num);
-            }
-            
-            else if(op=="/") {
-
-                result=result/roman_to_integer(num);
-            }
-            
-            else {
-                cout << "error";
-            }
+        } else{
+             result = calculator(num, result, op);
         }
     }
-
-    cout << integer_to_Roman(result) << endl;
-    
+    cout << intToRoman(result) << endl;
 
     return 0;
-}
+}           
